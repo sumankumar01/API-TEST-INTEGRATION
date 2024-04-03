dc9d89bb373138d2a0626e65ff9d6c73713956fe
suman kumar <suman1ew09@gmail.com>
2024-04-03 17:08:02 +0530

    first commit 6

--- src/test/java/org/example/apiStepDef/ReqResStepDef.java
+++ src/test/java/org/example/apiStepDef/ReqResStepDef.java
@@ -1,22 +1,13 @@
 package org.example.apiStepDef;
 
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
-import io.restassured.specification.RequestSpecification;
-import net.thucydides.core.annotations.Step;
+
 import net.thucydides.core.annotations.Steps;
-import org.awaitility.Awaitility;
 import org.example.apiSteps.StepsApi;
 import org.example.apiUtil.Util;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Named;
 
-import java.io.FileNotFoundException;
-import java.time.Duration;
-import java.util.concurrent.Callable;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Predicate;
+
 
 public class ReqResStepDef {
 
@@ -37,40 +28,11 @@ public class ReqResStepDef {
     @Given("I am fetching user list")
     public void fetchUserList()  {
 
-        verifyStatus(ReqResStepDef::apistatus,2,3,20);
+        stepsApi.verifyStatus(StepsApi::apistatus,2,3,20);
     }
 
-    public void verifyStatus(Predicate<Response> predicate, int delay, int pollInterval, int timeout )
-    {
-        Callable<Response> callable= () -> getuserList();
-
-        Response response=Awaitility.await().pollInSameThread()
-                .pollDelay(Duration.ofSeconds(delay))
-                .pollInterval(Duration.ofSeconds(pollInterval))
-                .atMost(Duration.ofSeconds(timeout))
-                .until(callable,predicate);
-
-        response.prettyPrint();
 
 
-
-    }
-    public static boolean apistatus(Response response)
-    {
-        ResponseBody responseBody=response.getBody();
-        int status=response.getStatusCode();
-
-        return  status == 200;
-    }
-
-    public Response getuserList()
-    {
-
-        Response response= RestAssured.given().log().all()
-                .when()
-                .get("https://reqres.in/api/users?delay=3");
-        return response;
-    }
 
 
     }
--- src/test/java/org/example/apiSteps/StepsApi.java
+++ src/test/java/org/example/apiSteps/StepsApi.java
@@ -2,10 +2,14 @@ package org.example.apiSteps;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
 import net.thucydides.core.annotations.Step;
 import org.awaitility.Awaitility;
 
+import java.time.Duration;
+import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
 
 public class StepsApi {
 
@@ -24,8 +28,40 @@ public class StepsApi {
                             .when()
                             .get("https://reqres.in/api/users?page="+page);
                     response.statusCode();
+                    response.prettyPrint();
                     return response.statusCode() == 200;
 
                 });
+    }
+    @Step("fetching the user detail list by polling")
+    public void verifyStatus(Predicate<Response> predicate, int delay, int pollInterval, int timeout)
+    {
+        Callable<Response> callable= () -> getuserList();
+
+        Response response=Awaitility.await().pollInSameThread()
+                .pollDelay(Duration.ofSeconds(delay))
+                .pollInterval(Duration.ofSeconds(pollInterval))
+                .atMost(Duration.ofSeconds(timeout))
+                .until(callable,predicate);
+
+        response.prettyPrint();
+
+
+    }
+    public static boolean apistatus(Response response)
+    {
+        ResponseBody responseBody=response.getBody();
+        int status=response.getStatusCode();
+
+        return  status == 400;
+    }
+
+    public Response getuserList()
+    {
+
+        Response response= RestAssured.given().log().all()
+                .when()
+                .get("https://reqres.in/api/users?delay=3");
+        return response;
     }
 }
